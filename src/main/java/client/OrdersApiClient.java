6a8bd76b1163baa99b3f4c5bbbe3da76508b2fe9
iSolov <isegos97@gmail.com>
2022-06-01 20:48:09 +0300

    Добавлены теста создания заказа

--- src/main/java/client/OrdersApiClient.java
+++ src/main/java/client/OrdersApiClient.java
@@ -3,11 +3,19 @@ package client;
 import static io.restassured.RestAssured.given;
 
 import io.restassured.response.Response;
+import models.AuthInfo;
 import models.OrderParameters;
 
 public class OrdersApiClient extends BaseHttpClient {
+    public Response makeOrder(OrderParameters orderParameters, AuthInfo authInfo) {
+        return given()
+                .header("Content-type", HEADER_CONTENT_TYPE)
+                .auth().oauth2(authInfo.getAccessToken())
+                .body(orderParameters.toJson())
+                .post("orders");
+    }
 
-    public Response makeOrder(OrderParameters orderParameters) {
+    public Response makeOrderWithoutAuth(OrderParameters orderParameters) {
         return given()
                 .header("Content-type", HEADER_CONTENT_TYPE)
                 .body(orderParameters.toJson())
--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -3,13 +3,18 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import client.BaseHttpClient;
 import client.IngredientsApiClient;
 import client.OrdersApiClient;
+import client.UsersApiClient;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import java.util.ArrayList;
 import java.util.List;
+
+import models.AuthInfo;
 import models.Ingredient;
 import models.OrderParameters;
+import models.User;
 import org.apache.http.HttpStatus;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,10 +23,16 @@ public class OrderTest {
 
     private final OrdersApiClient ordersApiClient = new OrdersApiClient();
     private final IngredientsApiClient ingredientsApiClient = new IngredientsApiClient();
+    private final UsersApiClient usersApiClient = new UsersApiClient();
 
     @Before
     public void setUp() {
         RestAssured.baseURI = BaseHttpClient.API_HOST;
+    }
+
+    @After
+    public void afterTest() {
+        usersApiClient.deleteCreatedUsers();
     }
 
     private List<Ingredient> getIngredients() {
@@ -30,12 +41,39 @@ public class OrderTest {
             .getList("data", Ingredient.class);
     }
 
+    private AuthInfo getRandomUserAuthInfo() {
+        return
+                usersApiClient
+                        .register(User.getRandomUser())
+                        .as(AuthInfo.class);
+    }
+
     @Test
-    @DisplayName("Должна быть возможность создать заказ.")
-    public void shouldLoginTest() {
+    @DisplayName("Должна быть возможность создать заказ для пользователя с авторизацией.")
+    public void shouldMakeOrderTest() {
         List<Ingredient> ingredients = getIngredients();
 
-        if (ingredients.size() == 0){
+        if (ingredients.size() == 0) {
+            Assert.fail("Отсутствуют ингредиенты для возможности создать заказ.");
+        }
+
+        ArrayList<String> orderIngredients = new ArrayList<>();
+        orderIngredients.add(ingredients.get(0).getId());
+
+        AuthInfo authInfo = getRandomUserAuthInfo();
+
+        ordersApiClient
+                .makeOrder(new OrderParameters(orderIngredients), authInfo)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .and().assertThat().body("success", equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Должна быть возможность создать заказ без авторизации.")
+    public void shouldMakeOrderWithoutAuthTest() {
+        List<Ingredient> ingredients = getIngredients();
+
+        if (ingredients.size() == 0) {
             Assert.fail("Отсутствуют ингредиенты для возможности создать заказ.");
         }
 
@@ -43,8 +81,32 @@ public class OrderTest {
         orderIngredients.add(ingredients.get(0).getId());
 
         ordersApiClient
-            .makeOrder(new OrderParameters(orderIngredients))
+                .makeOrderWithoutAuth(new OrderParameters(orderIngredients))
                 .then().assertThat().statusCode(HttpStatus.SC_OK)
                 .and().assertThat().body("success", equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Должна быть ошибка при заказе без ингредиентов.")
+    public void shouldGetErrorWhenMakeOrderWithoutIngredientsTest() {
+        AuthInfo authInfo = getRandomUserAuthInfo();
+
+        ordersApiClient
+                .makeOrder(new OrderParameters(new ArrayList<>()), authInfo)
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .and().assertThat().body("success", equalTo(false));
+    }
+
+    @Test
+    @DisplayName("Должна быть ошибка при заказе без ингредиентов.")
+    public void shouldGetErrorWhenMakeOrderWithWrongIngredientTest() {
+        AuthInfo authInfo = getRandomUserAuthInfo();
+
+        ArrayList<String> orderIngredients = new ArrayList<>();
+        orderIngredients.add("61c0c5a71d1f82001_______");
+
+        ordersApiClient
+                .makeOrder(new OrderParameters(orderIngredients), authInfo)
+                .then().assertThat().statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR);
     }
 }
